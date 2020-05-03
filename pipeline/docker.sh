90deb61f291a699413f91af90420594ada7abb03
Rafael Alberto <rafael.alberto1703@gmail.com>
2020-05-03 16:46:50 -0300

    improving docker.sh

--- pipeline/docker.sh
+++ pipeline/docker.sh
@@ -1,25 +1,25 @@
-echo "*** Delete container if exists ***"
-docker ps -q -f name=timesheet-api
-if [ $? -eq 0 ];
-then
-     echo "*** Remove container ***"
-     docker rm -f timesheet-api
-fi
+#echo "*** Delete container if exists ***"
+#docker ps -q -f name=timesheet-api
+#if [ $? -eq 0 ];
+#then
+#     echo "*** Remove container ***"
+#     docker rm -f timesheet-api
+#fi
+#
+#echo "*** Delete image if exists ***"
+#docker images -q timesheet-api
+#if [ $? -eq 0 ];
+#then
+#     echo "*** Remove image ***"
+#     docker rmi -f timesheet-api
+#fi
 
-echo "*** Delete image if exists ***"
-docker images -q timesheet-api
-if [ $? -eq 0 ];
-then
-     echo "*** Remove image ***"
-     docker rmi -f timesheet-api
-fi
-
-docker login --username rafaalberto17 --password 'dockertest'
+#docker login --username rafaalberto17 --password 'dockertest'
 
 echo "*** Docker create image ***"
-docker build . -t timesheet-api
+docker build -t timesheet-api .
 
 echo "*** Docker create container ***"
 docker run -d --name timesheet-api -e APP_OPTIONS='--spring.datasource.url=jdbc:postgresql://localhost:5432/timesheet' -p 8080:8080 --network=host timesheet-api
 
-docker logout
+#docker logout
