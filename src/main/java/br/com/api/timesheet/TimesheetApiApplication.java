f051e0c8257b510f89c4dabffa1becfa62085252
Rafael Alberto <rafael.alberto1703@gmail.com>
2021-08-30 21:06:04 -0300

    [feature/profiles] - created profiles properties, add databaseMigration more than one database and fix dockersh file

--- src/main/java/br/com/api/timesheet/TimesheetApiApplication.java
+++ src/main/java/br/com/api/timesheet/TimesheetApiApplication.java
@@ -2,8 +2,10 @@ package br.com.api.timesheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active}.properties")
 public class TimesheetApiApplication {
 
   public static void main(String[] args) {
