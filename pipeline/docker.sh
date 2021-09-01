64bfcb7a295d32be06adf2db03e63a190fdf3fdd
Rafael Alberto <rafael.alberto1703@gmail.com>
2021-08-31 22:41:29 -0300

    fix migrationsh

--- pipeline/docker.sh
+++ pipeline/docker.sh
@@ -7,19 +7,19 @@ then
 fi
 
 echo "*** Delete image if exists ***"
-docker images -q timesheet-api
+docker images -q rafaalberto17/timesheet:timesheet
 if [ $? -eq 0 ];
 then
      echo "*** Remove image ***"
-     docker rmi -f timesheet-api
+     docker rmi -f rafaalberto17/timesheet:timesheet
 fi
 
 echo "*** Docker create image ***"
-sudo docker build -t timesheet-api .
+docker build -t timesheet-api .
 
 docker logout
 docker login --username rafaalberto17 --password=raf17prog89alb03
 docker pull rafaalberto17/timesheet:timesheet
 
 echo "*** Docker create container ***"
-sudo docker run -d --name timesheet-api -e APP_OPTIONS='--spring.profiles.active=local' -p 8080:8080 --network=host rafaalberto17/timesheet:timesheet
+docker run -d --name timesheet-api -e APP_OPTIONS='--spring.profiles.active=local' -p 8080:8080 --network=host rafaalberto17/timesheet:timesheet
